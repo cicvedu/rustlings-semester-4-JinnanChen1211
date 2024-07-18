b22261806156c6145261bb8018353a16fd8738a1
chenbo <chenb@aeroht.com>
2024-07-18 11:33:29 +0800

    [Update] Complete five hash & module & strings.

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,22 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
-}
 
-fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
-}
+  fn trim_me(input: &str) -> String {
+      // TODO: Remove whitespace from both ends of a string!
+      input.trim().to_string()
+  }
 
-fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
-}
+  fn compose_me(input: &str) -> String {
+      // TODO: Add " world!" to the string! There's multiple ways to do this!
+      format!("{} world!", input)
+  }
+
+  fn replace_me(input: &str) -> String {
+      // TODO: Replace "cars" in the string with "balloons"!
+      input.replace("cars", "balloons")
+  }
+
+
 
 #[cfg(test)]
 mod tests {
